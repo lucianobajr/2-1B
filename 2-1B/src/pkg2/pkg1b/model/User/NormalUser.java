0f16ada23aa7ddea8485fff02fd35d9c603e97a7
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-05-16 11:34:06 -0300

    normalUser
    estou trabalhando na normal user

--- 2-1B/src/pkg2/pkg1b/model/User/NormalUser.java
+++ 2-1B/src/pkg2/pkg1b/model/User/NormalUser.java
@@ -9,21 +9,15 @@ import pkg2.pkg1b.model.Enviroment.Enviroment;
 
 public class NormalUser extends User {
     private int Desease_history_score; 
-    public NormalUser(String name, int age,Sex sex) {
-        super(name, age,sex);
-    } 
-
-public class EmployedUser extends User{ 
-    private int Desease_history_score;  
     // define a mascara que o usuario está usando 
     Mask TypeMask = new Mask();   
     // define o ambiente  que o usuario esta
     Enviroment UserEnv = new Enviroment(); 
      
-
-    public EmployedUser(String name, int age, Sex sex){   
-        super(name,age,sex); 
-    }   
+    public NormalUser(String name, int age,Sex sex) {
+        super(name, age,sex);
+    } 
+     
     public void init_NormalUser_verification(){  
         Scanner getK = new Scanner(System.in); 
         //System.out.println("Digite 1 para sim ou 2 para não para cada uma das doenças abaixo"); 
@@ -45,7 +39,7 @@ public class EmployedUser extends User{
             } 
             
         }  
-    } 
+     
    
     public void show_user(){ 
         System.out.println("---------------------------------");
@@ -59,10 +53,10 @@ public class EmployedUser extends User{
         System.out.println("---------------------------------");
 
     } 
-    private String toStringEmployed(){  
+    private String toStringNormalUser(){  
         // se der problema de linha a mais tirar o /n
         return getName()+" "+getAge()+" "+getSex()+" "+this.Desease_history_score +"\n"; 
-    }  
+    } 
 
   
 }
